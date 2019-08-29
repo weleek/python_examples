dc86926de840b77b6b8aacf1d27102be3ac6d68b
JongHyun Lee <jonghyunlee@JongHyuns-iMac.local>
2019-08-29 10:24:07 +0900

    shell print color 설정 값 테이블 보기 예제

---
+++ default/print_color.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+
+def print_format_table():
+    """
+    prints table of formatted text format options
+    """
+    for style in range(8):
+        for fg in range(30,38):
+            s1 = ''
+            for bg in range(40,48):
+                format = ';'.join([str(style), str(fg), str(bg)])
+                s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
+            print(s1)
+        print('\n')
+
+# color_string = "\x1b["+ "7;37;40" + "word"  + "\x1b[0m"
+
+print_format_table()
+
